e680cd3c18f0218142ca28d332c7b63630e837d3
Kevin sinnadurai <sinnadurai.kevin@hotmail.fr>
2021-11-14 19:22:55 +0100

    serveur.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-  res.sendFile('render.html',{root:__dirname});
+  res.sendFile('index1.html',{root:__dirname});
 });
 
 app.listen(3001, () => {
